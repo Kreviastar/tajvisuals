0f4e5514db128f84c8d037ce533572af81e8bfbe
Kreviastar <krevia2025@outlook.com>
2025-12-10 03:03:33 -0800

    Update configs and base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: "/Pro-Portfolio/"  // <--- THIS is the important part
+   base: "/tajvisuals"  // <--- THIS is the important part
 })
